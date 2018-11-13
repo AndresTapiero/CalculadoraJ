5c365e741ee4976ead2ea54f02c6835aeafcf24f
AndresTapiero <andrestapiero72@gmail.com>
2018-11-13 18:44:36 -0500

    Clean code

--- app/src/main/java/com/example/andrestapiero/calculadoraj/MainActivity.java
+++ app/src/main/java/com/example/andrestapiero/calculadoraj/MainActivity.java
@@ -6,165 +6,159 @@ import android.view.View;
 import android.widget.TextView;
 
 public class MainActivity extends AppCompatActivity {
-    int operador = 0;
-    private TextView textViewresultado;
-    private double result, number1, number2;
+    private int operator;
+    private TextView textViewResult;
+    private double number1, number2;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
-        textViewresultado = (TextView) findViewById(R.id.resultado);
+        textViewResult = findViewById(R.id.resultado);
     }
 
-    public void OnClickListenerNumbers(View view) {
-        String inputNumber = textViewresultado.getText().toString();
+    private void setOperator (int operator) {
+        this.operator = operator;
+    }
+
+    private int getOperator() {
+        return this.operator;
+    }
+
+    private double getFirstNumber() {
+        return this.number1;
+    }
+
+    private double getSecondNumber() {
+        return this.number2;
+    }
+
+    private void setFirstNumber(double number) {
+        this.number1 = number;
+    }
+
+    private void setSecondNumber(double number) {
+        this.number2 = number;
+    }
+
+    public void onClickListenerNumbers(View view) {
+        String inputNumber = textViewResult.getText().toString();
         String selectedNumber = "";
 
         switch (view.getId()) {
             case R.id.btn0:
                 selectedNumber = "0";
                 break;
-
             case R.id.btn1:
                 selectedNumber = "1";
                 break;
-
             case R.id.btn2:
                 selectedNumber = "2";
                 break;
-
             case R.id.btn3:
                 selectedNumber = "3";
                 break;
-
             case R.id.btn4:
                 selectedNumber = "4";
                 break;
-
             case R.id.btn5:
                 selectedNumber = "5";
                 break;
-
             case R.id.btn6:
                 selectedNumber = "6";
                 break;
-
             case R.id.btn7:
                 selectedNumber = "7";
                 break;
-
             case R.id.btn8:
                 selectedNumber = "8";
                 break;
-
             case R.id.btn9:
                 selectedNumber = "9";
                 break;
-
             case R.id.btnpoint:
-                if (!isResultNumberEmpty() && !textViewresultado.getText().toString().contains(".")) {
+                if (!isResultNumberEmpty() && !textViewResult.getText().toString().contains(".")) {
                     selectedNumber = ".";
                 }
                 break;
         }
-        textViewresultado.setText(inputNumber.concat(selectedNumber));
+        textViewResult.setText(inputNumber.concat(selectedNumber));
     }
 
     private Boolean isResultNumberEmpty() {         //Metodo que compara si el String esta vacio
-        return textViewresultado.getText().toString().equals("");
+        return textViewResult.getText().toString().equals("");
     }
 
-
-
-    public void OnClickListenerOperators(View view) {
+    public void onClickListenerOperators(View view) {
+        int selectedOperator = 0;
 
         switch (view.getId()) {
             case R.id.btnSuma:
-                if (!isResultNumberEmpty()) {
-                    GetFirstNumber();
-                    operador = 1;
-                }
+                selectedOperator = 1;
                 break;
-
             case R.id.btnMenos:
-                if (!isResultNumberEmpty()) {
-                    GetFirstNumber();
-                    operador = 2;
-                }
+                selectedOperator = 2;
                 break;
-
             case R.id.btnmtl:
-                if (!isResultNumberEmpty()) {
-                    GetFirstNumber();
-                    operador = 3;
-                }
+                selectedOperator = 3;
                 break;
-
             case R.id.btndv:
-                if (!isResultNumberEmpty()) {
-                    GetFirstNumber();
-                    operador = 4;
-                }
+                selectedOperator = 4;
                 break;
             case R.id.btnpor:
-                if (!isResultNumberEmpty()){
-                    GetFirstNumber();
-                    operador = 5;}
+                selectedOperator = 5;
                 break;
         }
-        textViewresultado.setText("");
+        saveFirstNumber();
+        setOperator(selectedOperator);
+        textViewResult.setText("");
     }
 
-    private double GetFirstNumber(){
-            String textnumber1;
-            textnumber1 = textViewresultado.getText().toString();
-            return number1= Double.parseDouble(textnumber1);
-    }
-
-    public void OnClickListenerEqual(View view) {
-
-        if (!isResultNumberEmpty()) {
-            String textnumber2 = textViewresultado.getText().toString();
-            number2 = Double.parseDouble(textnumber2);
-            switch (operador) {
-                case 1:
-                    result = number1 + number2;
-                    break;
-
-                case 2:
-                    result = number1 - number2;
-                    break;
-
-                case 3:
-                    result = number1 * number2;
-                    break;
-
-                case 4:
-                    result = number1 / number2;
-                    break;
-
-                case 5:
-                    result = number2 * (number1 / 100.0);
-                    break;
-            }
-            textViewresultado.setText("" + result);
-            number1 = result;
+    private void saveFirstNumber(){
+        String textNumber1 = textViewResult.getText().toString();
+        if (!textNumber1.equals("")) {
+            setFirstNumber(Double.parseDouble(textNumber1));
         }
     }
 
-    public void OnClickListenerClear(View view) {
+    public void onClickListenerEqual(View view) {
+        double result = 0;
+        if (!isResultNumberEmpty()) {
+            String textNumber2 = textViewResult.getText().toString();
+            setSecondNumber(Double.parseDouble(textNumber2));
+            switch (getOperator()) {
+                case 1:
+                    result = getFirstNumber() + getSecondNumber();
+                    break;
+                case 2:
+                    result = getFirstNumber() - getSecondNumber();
+                    break;
+                case 3:
+                    result = getFirstNumber() * getSecondNumber();
+                    break;
+                case 4:
+                    result = getFirstNumber() / getSecondNumber();
+                    break;
+                case 5:
+                    result = getSecondNumber() * (getFirstNumber() / 100.0);
+                    break;
+            }
+            textViewResult.setText(""+result);
+            setFirstNumber(result);
+        }
+    }
+
+    public void onClickListenerClear(View view) {
         switch (view.getId()) {
 
             case R.id.btndelete:
-                textViewresultado.setText("");
-                number1 = 0.0;
-                number2 = 0.0;
+                textViewResult.setText("");
+                setFirstNumber(0.0);
+                setSecondNumber(0.0);
                 break;
-
             case R.id.btndel:
                 if (!isResultNumberEmpty()) {
-                    textViewresultado.setText(textViewresultado.getText().subSequence(0, textViewresultado.getText().length() - 1) + "");
+                    textViewResult.setText(textViewResult.getText().subSequence(0, textViewResult.getText().length() - 1));
                 }
         }
     }
